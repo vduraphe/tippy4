2e5e14ede6af4c2a18a46834338f297789c2429f
Tai <islandbreeze5647@gmail.com>
2016-12-22 23:20:05 -0800

    Initial Commit

---
+++ tippy4/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  tippy4
+//
+//  Created by Vaidehi Duraphe on 12/22/16.
+//  Copyright © 2016 Vaidehi Duraphe. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
